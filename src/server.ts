952cd1284a307b5be6969cbe27d65a06fa76c6ff
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-02-22 18:55:58 -0300

    feat: create api routes

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  return res.json('Hello World - NLW4');
+})
+
+app.post('/', (req, res) => {
+  return res.json({ message: 'Dados salvos com sucesso!' });
+})
+
 app.listen(3333, () => console.log('Server is running!'));
