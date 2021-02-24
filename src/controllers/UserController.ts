f9820fbd562d1587b51bde7fd12327a9fc050b0d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-02-24 09:53:17 -0300

    feat: add user create action

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,27 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
 
   async create(request: Request, response: Response) {
-    const body = request.body;
-    console.log(body);
-    return response.send();
+    const { name, email} = request.body;
+
+    const userRepository = getRepository(User);
+
+    const userAlereadyExists = await userRepository.findOne({ email });
+
+    if (userAlereadyExists) {
+      return response.status(400).json({ error: 'User already exists!'});
+    }
+
+    const user = userRepository.create({
+      name, email
+    });
+
+    await userRepository.save(user);
+
+    return response.json(user);
   }
 
 }
