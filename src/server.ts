8d478694cbb32b1190c5407abdc85b1aa491da7b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-02-24 21:26:07 -0300

    chore:  add adress and port on server console text

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,9 @@ import { router } from './routes';
 
 
 const app = express();
+const port = 3333;
 
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => console.log('Server is running!'));
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
